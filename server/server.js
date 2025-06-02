9cab3fe41649f145adc2d3c140fd07c4d228b4a5
meet-navapara <142219118+meet-navapara@users.noreply.github.com>
2025-06-02 13:25:32 +0530

    change in server file

--- server/server.js
+++ server/server.js
@@ -20,13 +20,16 @@ app.use('/api/payment', cors(), require("./routes/payment"))
 
 
 // Serve frontend in production
+const path = require('path'); // Make sure this is at the top with other requires
+
+// Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder for the React build
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   // Any requests that are not API routes should be redirected to the React app
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
 
