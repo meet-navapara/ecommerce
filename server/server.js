01e9965866d9cd44911288a9439e1532ae51bc7f
meet-navapara <142219118+meet-navapara@users.noreply.github.com>
2025-06-02 12:28:41 +0530

    change in server file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors")
 require("dotenv").config()
 const connectDB = require("./config/db")
+const path = require('path'); // Make sure this line is at the top with other requires
 const PORT = process.env.PORT || 5000
 
 // middlewares
@@ -15,5 +16,18 @@ app.use(express.static("public"));
 
 app.use('/api/items', require("./routes/items"))
 app.use('/api/payment', cors(), require("./routes/payment"))
+
+
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder for the React build
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // Any requests that are not API routes should be redirected to the React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, console.log("Server is running on port ", PORT))
